5e64a7610096943e995f46f1e630a6b280971595
dhertzb <dhertzb@gmail.com>
2021-04-17 19:49:55 -0300

    [add] adicionado prisma orm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,22 @@
-
+import { PrismaClient } from '@prisma/client'
 import express from 'express'
 import routes from './routes'
+
+const prisma = new PrismaClient()
+
 const main = async () => {
 
     const app = express();
-    
+    await prisma.user.create({
+      data: {
+        name: 'Alice',
+        email: 'alice@prisma.io'
+      }
+    })
+  
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
+
     app.use(routes)
 
     app.listen(3333, () =>{
@@ -13,3 +25,9 @@ const main = async () => {
 };
 
 main()
+.catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
