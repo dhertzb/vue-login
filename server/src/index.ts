56cad693075f272a261938848ed610eb9f4f4790
dhertzb <dhertzb@gmail.com>
2021-04-23 23:37:08 -0300

    [create] seeds

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,15 @@
-import { PrismaClient } from '@prisma/client'
 import express from 'express'
 import routes from './routes'
-
-const prisma = new PrismaClient()
 
 const main = async () => {
 
     const app = express();
-    await prisma.user.create({
-      data: {
-        name: 'Alice',
-        email: 'alice@prisma.io'
-      }
-    })
-  
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
 
     app.use(routes)
 
     app.listen(3333, () =>{
-        console.log(3)
+        console.log('App just initialize in localhost:3333')
     })
 };
 
 main()
-.catch(e => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
