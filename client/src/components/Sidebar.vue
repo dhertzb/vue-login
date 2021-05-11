96219a4c837dc5c371eea48e6af3cf6fbbd1bf5f
dhertzb <dhertzb@gmail.com>
2021-05-10 22:28:30 -0300

    [add] logout

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-navigation-drawer v-model="isSidebarOpen" absolute temporary>
+  <v-navigation-drawer v-model="isSidebarOpen" absolute>
     <v-list nav dense>
-      <v-list-item-group
-        v-model="group"
-        active-class="deep-purple--text text--accent-4"
-      >
+      <v-list-item-group>
+        <v-app-bar-nav-icon @click="toggleSidebar" />
+      </v-list-item-group>
+
+      <v-list-item-group v-model="group">
         <v-list-item>
           <v-list-item-icon>
             <v-icon>mdi-home</v-icon>
@@ -20,6 +21,16 @@
         </v-list-item>
       </v-list-item-group>
     </v-list>
+    <v-list nav dense>
+      <v-list-item-group>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-logout-variant</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title @click="logout">Logout</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
   </v-navigation-drawer>
 </template>
 <script lang="ts">
@@ -30,5 +41,21 @@ import { mapGetters } from "vuex";
 @Component({
   computed: mapGetters(["isSidebarOpen"]),
 })
-export default class Sidebar extends Vue {}
+export default class Sidebar extends Vue {
+  toggleSidebar() {
+    this.$store.commit("TOGGLE_SIDEBAR", !this.isSidebarOpen);
+  }
+  logout() {
+    this.$store.dispatch("logout").then(() => {
+      this.$router.push("/login");
+    });
+  }
+}
 </script>
+<style>
+.v-navigation-drawer__content {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+</style>
